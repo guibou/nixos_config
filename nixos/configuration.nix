c88a62ed4f87b1b4f51dad535df8978a578010b3
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2024-04-21 12:22:06 +0400

    Formatting configuration.nix and cleaning really old things

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,15 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, nixpkgs, lib, disko, ... }:
 
 {
-  imports = [ # Include the results of the hardware scan.
+  imports = [
+    # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    # Disable system wide sway, maybe I'll give it another try in the future
-    # using home-manager setup
-    # ./sway.nix
     "${disko}/module.nix"
     ./disko.nix
     ./camera.nix
@@ -33,56 +27,31 @@
       efi.canTouchEfiVariables = false;
     };
 
-    #binfmt.emulatedSystems = ["armv6l-linux" "armv7l-linux"];
-
+    # Boot is not on tmpfs because I had problem when installing large package
+    # But cleaning tmp on boot allows the cruft to not stay.
     tmp.cleanOnBoot = true;
 
     # save batter in sleep
-    # This was needed by XPS 13, not anymore on lenovo X1 Gen 8
+    # That's XPS only
     kernelParams = [ "mem_sleep_default=deep" ];
   };
 
-  # systemd version of earlyoom
-  # Compare with earlyoom and facebook oomd?
+  # TODO: this is in order to kill programs, such as ghc, when it uses too much memory.
+  # But i had never had really good results with it, my system is still freezing sometime.
   systemd.oomd.enable = true;
 
   networking = {
-    hostName = "gecko"; # Define your hostname.
+    hostName = "gecko";
+
     networkmanager = {
       enable = true;
-
-      # Remove a lot of vpn plugins
-      plugins = lib.mkForce [];
     };
-    # allow wifi in cafee
+
+    # Allow wifi in cafee
     resolvconf.dnsExtensionMechanism = false;
 
     firewall = {
       enable = true;
-
-      # Open ports in the firewall.
-      # for koryo
-      # allowedTCPPorts = [ 3003 3004 9160 ];
-
-      # For castnow
-      allowedTCPPortRanges = [
-        # Castnow
-        {
-          from = 4100;
-          to = 4105;
-        }
-        # Koryo
-        {
-          from = 3003;
-          to = 3004;
-        }
-	# Webserver
-	{
-	   from = 8000;
-	   to = 8081;
-	}
-      ];
-      allowedUDPPorts = [ 5353 ];
     };
   };
 
@@ -95,16 +64,14 @@
   i18n = { defaultLocale = "en_US.UTF-8"; };
 
   #time.timeZone = "Europe/Paris";
-  # time.timeZone = "Indian/Reunion";
-  #time.timeZone = "Indian/Mauritius";
-
+  #time.timeZone = "Indian/Reunion";
   # "Indian/Reunion" does not work with firefox, I have no idea why.
   time.timeZone = "Asia/Dubai";
-  # time.timeZone = "America/Los_Angeles";
 
   documentation.man.enable = true;
 
-  fonts.packages = with pkgs; [ noto-fonts monaspace (nerdfonts.override { fonts = [ "BitstreamVeraSansMono" ]; })  ];
+  # TODO: Reevaluate if this cannot be moved to home-manager
+  fonts.packages = with pkgs; [ noto-fonts monaspace (nerdfonts.override { fonts = [ "BitstreamVeraSansMono" ]; }) ];
 
   # services.nix-serve.enable = true;
 
@@ -112,15 +79,9 @@
     bash.enableCompletion = true;
     zsh.enable = true;
     ssh.startAgent = true;
-    #gnome-terminal.enable = true;
   };
 
-  # Enable integration with google services in gnome
-  # services.gnome.gnome-online-accounts.enable = true;
-  # services.gnome.gnome-keyring.enable = true;
-  # services.gvfs.enable = true;
-  # programs.seahorse.enable = true;
-
+  # Automatic change screen configuration
   services.autorandr.enable = true;
 
   services = {
@@ -134,19 +95,22 @@
 
     logind = { lidSwitch = "ignore"; };
     avahi.enable = false;
+
     # fingerprint things. I don't like it.
     # fprintd.enable = true;
 
     fwupd.enable = true; # BIOS upgrade
     fstrim.enable = true; # trim for SSD
-    openssh.enable = true;
+
+    # I'm NOT connecting to SSH on my laptop
+    openssh.enable = false;
+
     timesyncd.enable = true; # NTP sync
     blueman.enable = true;
 
     # Enable the X11 windowing system.
     xserver = {
       enable = true;
-      # videoDrivers = ["intel"];
 
       # Enable touchpad support.
       libinput.enable = true;
@@ -156,34 +120,20 @@
         layout = "us";
       };
 
-      #dpi = 200;
-
+      # TODO: this could be moved to home-manager
       windowManager.i3 = {
         enable = true;
         extraPackages = with pkgs; [ dmenu i3lock i3status ];
       };
 
-      # displayManager.lightdm.enable = true;
-      #displayManager.gdm.wayland.enable = true;
       displayManager.gdm.enable = true;
-      #displayManager.gdm.wayland = true;
-
-      # gnome 3
-      # desktopManager.gnome3.enable = true;
     };
 
-    # Kill program before they eat all my memory
-    # I can also use magic sysrq
-    # On dell XPS:
+    # Note: DELL XPS system rescue keys
     # - Press <Alt>, then <Fn>, then R key
     # - release R key, and press
     # - f (on the keyboard, not your layout) to run the OOM killer
-    # earlyoom.enable = true;
-
-    # Allows CPU to burn
-    # This was needed for xps. I'm testing without it on the lenovo
-    # throttled.enable = true;
-
+    # - s(ync) / u(mount) / b(boot)
   };
 
   sound = { enable = true; };
@@ -202,12 +152,10 @@
       "wheel"
       "docker"
       "plugdev"
-      "vboxusers"
       "networkmanager"
       "video" # for brightness control
-      "adbusers"
-      "dialout" # for arduino
     ];
+
     shell = pkgs.zsh;
 
     hashedPassword =
@@ -222,148 +170,59 @@
     docker.enable = false;
     docker.enableOnBoot = false;
     docker.autoPrune.enable = true;
-
-    #virtualbox.host.enable = true;
-    virtualbox.host.enableHardening = false;
   };
 
-  #networking.interfaces.vboxnet0.ipv4.addresses = lib.mkForce [{ address = "192.168.254.1"; prefixLength = 24; }];
-
   nix = {
-    /*
-    registry.nixpkgs.to = {
-      type = "path";
-      path = pkgs.path;
-    };
-    */
+    # Pin registry so it only contains flake and that this flake is already
+    # pinned to current nixpkgs.
+
+    # This saves a nixpkgs redownload everytime I run `nix run nixpkgs#xxx`,
+    # which is highly convenient when you are on the other side of the world
+    # and the internect connection is going through an optical fiber under the
+    # ocean that rocks and sharks are cutting every days ending by "day".
     registry.nixpkgs.flake = nixpkgs;
 
     package = pkgs.nixUnstable;
-    buildMachines = [
-      {
-        hostName = "ovh-hybrid-runner-1.devops.novadiscovery.net";
-        maxJobs = 47;
-        system = "x86_64-linux";
-        supportedFeatures = [ "big-parallel" "benchmark" "kvm" ];
-      }
-      {
-        hostName = "ovh-hybrid-runner-2.devops.novadiscovery.net";
-        maxJobs = 47;
-        system = "x86_64-linux";
-        supportedFeatures = [ "big-parallel" "benchmark" "kvm" ];
-      }
-    ];
-
-    extraOptions = ''
-      # I had problems with auto-optimise performance when doing GC
-      # Instead, I think I'll regularly run `nix store optimise`
-      auto-optimise-store = false
-      builders-use-substitutes = true
-      # require-sigs = false
-      # post-build-hook = /etc/nix/post-build-hook.sh
-      extra-experimental-features = nix-command flakes recursive-nix
-      #extra-substituters = http://192.168.1.122:8080
-      # extra-substituters = s3://devops-ci-infra-prod-caching-nix?region=eu-central-1&profile=nix-daemon
-
-    #extra-substituters = s3://devops-ci-infra-prod-caching-nix?region=eu-central-1&profile=nix-daemon
-    #extra-trusted-public-keys = jinkotwo:04t6bF1/peQlZWVpYPN0BraxIV2pdlN2005Vi0hUvso=
-
-      warn-dirty = false
-      flake-registry =
-    '';
 
     settings = {
       sandbox = true;
       trusted-users = [ "root" "guillaume" ];
-      cores = 8;
-      max-jobs = 8;
+      cores = 12;
+      max-jobs = 12;
+
+      # This thing had saved me a lot of disk before, but `nix-collect-garbage`
+      # can be impressively slow and fail in case of not enough hard disk.
+      # It is also slower when doing nix build
+      # I'm now relying on manual optimisation on demand + btrfs compression +
+      # (TODO: btrfs deduplication).
+      # I can also nix-collect-garbage every year and not wait 3 years like
+      # last time ;)
+      auto-optimise-store = false;
+
+      builders-use-substitutes = true;
+      extra-experimental-features = [ "nix-command" "flakes" "recursive-nix" ];
+      warn-dirty = false;
+
+      # No more implicit registry
+      flake-registry = [ ];
     };
   };
 
+  # Required for some unfree firmware, I suppose.
   nixpkgs.config.allowUnfree = true;
 
-  nixpkgs.config.chromium = {
-    # enableVaapi = true;
-  };
-
   hardware = {
-    sane = {
-      enable = true;
-      brscan4 = {
-        enable = true;
-	netDevices = {
-          office = {
-	    ip = "192.168.1.9";
-	    model = "MFC-J6945DW";
-	  };
-	};
-      };
-    };
     cpu.intel.updateMicrocode = true;
-    pulseaudio = {
-      enable = false;
-      # Disabled for pipewire
-      # enable = false;
-
-      extraConfig = ''
-         # set-card-profile bluez_card.C0_86_B3_57_F3_DF handsfree_head_unit
-      '';
-    };
     opengl = {
       enable = true;
       driSupport = true;
+
+      # Not playing with 32bits things, so don't care for now.
       driSupport32Bit = false;
-
-      # Intel hw accelerated
-      # Deactivated. It generates a lot of glitches
-      extraPackages = with pkgs; [
-        #intel-media-driver # LIBVA_DRIVER_NAME=iHD
-        #vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-        #vaapiVdpau
-        #libvdpau-va-gl
-        #intel-ocl
-      ];
     };
+
     bluetooth.enable = true;
-
-    # video.hidpi.enable = true;
   };
 
-  # Setup recommanded power management settings
-  # TODO: I'm trying to disable that. I'm observing slow wifi detection, issues
-  # at wake up, ...
-  #powerManagement.powertop.enable = true;
   powerManagement.enable = true;
-
-  # For android developement
-  # programs.adb.enable = true;
-
-  #services.unifi.enable = true;
-
-  # Swap file on btrfs created using:
-  # touch /var/swapfile
-  # chmod 600 /var/swapfile
-  # chattr +C /var/swapfile
-  # fallocate /var/swapfile -l4g
-  # mkswap /var/swapfile 
-
-  # Swap is recommanded with systemd.oomd
-  #swapDevices = [{
-  #  device = "/var/swapfile";
-  #  size = 8192;
-  #}];
-
-  # services.redis.servers."".enable = true;
-  services.postgresql = {
-    enable = false;
-    package = pkgs.postgresql_12;
-  };
-
-  /*
-  xdg.portal = {
-   enable = true; 
-   xdgOpenUsePortal = true; 
-   wlr.enable = true;
-  };
-  */
 }
