69c1edb8426768ba6cf2cc4f6a716bcb3539ce4f
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2025-10-13 14:41:21 +0200

    feat(firefox): setup for password manager

--- home/firefox.nix
+++ home/firefox.nix
@@ -27,6 +27,12 @@
           "autoDisableScopes" = 0;
           "sidebar.verticalTabs" = true;
 
+          # Do not auto save on Download folder
+          "browser.download.useDownloadDir" = false;
+          "extensions.formautofill.addresses.enabled" = false;
+          "extensions.formautofill.creditCards.enabled" = false;
+          "signon.rememberSignons" = false;
+
           # TODO: session restore + addons automatic accept + remove all "startup popup"
         };
         extensions.packages = with nur.repos.rycee.firefox-addons; [
