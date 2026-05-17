c6a744ef9c884a251c308268d849413508521d03
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2026-05-17 16:24:19 +0400

    firefox: disable AI things

--- home/firefox.nix
+++ home/firefox.nix
@@ -39,6 +39,28 @@
           "browser.startup.homepage" = "about:blank";
           "browser.urlbar.placeholderName" = "DuckDuckGo";
 
+          # Disable AI things
+          # these flags actualy disable the AI features
+          # Found there: https://www.reddit.com/r/NixOS/comments/1rkt5ag/reminder_to_declare_the_blocking_of_ai_stuff_in/
+          # Note that this list may have to be updated in the future, this is
+          # sad that firefox does not provide a master switch
+          "browser.ml.chat.enabled" = false;
+          "browser.ml.chat.page" = false;
+          "browser.ml.linkPreview.enabled" = false;
+          "browser.tabs.groups.smart.enabled" = false;
+          "browser.tabs.groups.smart.userEnabled" = false;
+          "browser.translations.enable" = false;
+          "extensions.ml.enabled" = false;
+          "pdfjs.enableAltText" = false;
+
+          # these flags change the visual UI on the AI panel
+          "browser.ai.control.default" = "blocked";
+          "browser.ai.control.linkPreviewKeyPoints" = "blocked";
+          "browser.ai.control.pdfjsAltText" = "blocked";
+          "browser.ai.control.sidebarChatbot" = "blocked";
+          "browser.ai.control.smartTabGroups" = "blocked";
+          "browser.ai.control.translations" = "blocked";
+
           # TODO: session restore + addons automatic accept + remove all "startup popup"
         };
         extensions.packages = with nur.repos.rycee.firefox-addons; [
